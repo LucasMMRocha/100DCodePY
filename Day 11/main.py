5e1fc7e46694b1d93a05ccb8e388b95a159dcdca
Lucas Macedo Rocha <cbp.lmd@gmail.com>
2024-02-01 22:54:43 -0300

    Commit day 11

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,4 +1,4 @@
-#from art import logo
+from art import logo
 import random
 import os
 
@@ -7,17 +7,6 @@ def add(c1, list):
 
 def total(list):
   return sum(list)
-  
-logo = """
- .------.            _ n    _            _    _            _    
- |A_  _ |.          | |   | |          | |  (_)          | |   
- |( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
- | \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
- |  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
- `-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
-       |  \/ K|                            _/ |                
-       `------'                           |__/           
- """
 
 #Black Jack Game
 
@@ -27,7 +16,7 @@ while should_continue == True:
 
   question = input("Do you want to play a Black Jack game? 'y' for yes and 'n' for no: ").lower()
 
-  os.system('clear')
+  os.system('cls')
 
   if question == 'n':
     should_continue = False
@@ -51,7 +40,7 @@ while should_continue == True:
     print(f"Your cards are [{cards_p1}], current score: {total(cards_p1)}")
     print(f"Computer's first card: {cards_cp[0]}\n")
 
-    while total(cards_p1) < 21:
+    while total(cards_p1) <= 21:
 
       card_p1 = cards[random.randint(0, 12)]
 
@@ -64,22 +53,23 @@ while should_continue == True:
       if question_2 == 'n':
         break
 
-      card_p1 = cards[random.randint(0, len(cards))]
+      card_p1 = cards[random.randint(0, 12)]
 
       add(card_p1, cards_p1)
       
-      
-
       print(f"Your cards are {cards_p1}, current score: {total(cards_p1)}")
       print(f"Computer's first card: {cards_cp[0]}\n")
     
     print(f"Your final hand: {cards_p1}, final score: {total(cards_p1)}")
     print(f"Computer's final hand: {cards_cp}, final score: {total(cards_cp)}")
-    if total(cards_p1) <= 21 and total(cards_p1) < total(cards_cp):
+    
+    if total(cards_p1) <= 21 and total(cards_p1) > total(cards_cp):
+      print("You won!")
+    elif total(cards_p1) <= 21 and total(cards_cp) > 21:
       print("You won!")
     elif total(cards_cp) <= 21 and total(cards_cp) > total(cards_p1):
       print("You lost.")
     elif total(cards_p1) == total(cards_cp):
       print("DRAW")
-    else:
+    elif total(cards_p1) > 21:
       print("You went over. You lost...")
