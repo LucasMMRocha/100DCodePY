4a859985ca69d85a6345a4882a9cd58a56739f3b
Lucas Macedo Rocha <cbp.lmd@gmail.com>
2024-02-06 13:28:54 -0300

    Commit

---
+++ Day 15/day_15.py
@@ -0,0 +1,8 @@
+# Flavors
+
+# Expresso        Latte         Cappuccino
+# W - 50ml       W - 200ml       W - 250ml
+# C - 18g         C - 24g         C - 24g
+#                M - 150ml       M - 100ml
+
+#   $1.50          $2.50           $3.00
