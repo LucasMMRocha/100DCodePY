76b1e0c3a680b0f5527bd6a6e347a75e956e85bc
Lucas Macedo Rocha <cbp.lmd@gmail.com>
2024-02-19 22:15:41 -0300

    Commit

--- Day 16/FP/main.py
+++ Day 16/FP/main.py
@@ -23,3 +23,4 @@ while is_on:
         if coffee_maker.is_resource_sufficient(drink):
             if money_machine.make_payment(drink.cost) and money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
+
