68b2e68c42a72828c629270fb892cfdd4939e87e
Lucas Macedo Rocha <cbp.lmd@gmail.com>
2024-02-21 01:24:20 -0300

    Commit

--- Day 17/main.py
+++ Day 17/main.py
@@ -11,6 +11,11 @@ class User:
         self.following += 1
 
 
-user_1 = User("001", "angela")
+user_1 = User("001", "Lucas")
+user_2 = User("002", "Sarah")
 
-print(user_1.id)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
