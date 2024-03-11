f8fb9aa8c9748b58b2c65b971963fa3c969d9a17
lucasmmr <cbp.lmd@gmail.com>
2024-03-11 15:49:22 -0300

    Commit

--- Day 25/main.py
+++ Day 25/main.py
@@ -15,7 +15,7 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(data)
 
 # temp_list = data["temp"].to_list()
@@ -30,4 +30,36 @@ data = pandas.read_csv("weather_data.csv")
 # print(data.condition)
 
 # Get data in Row
-print(data[data.temp == data.temp.max()])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+#
+# monday_temp = monday.temp[0]
+# f_temp = monday_temp * (9 / 5) + 32
+#
+# print(f_temp)
+
+
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+data_dict = {
+    "Fur Color": ["Grey", "Red", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
